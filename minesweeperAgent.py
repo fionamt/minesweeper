e00c04cfbfd9a53225adb0e8b9fc1331616e296c
Alex Engel <atengel@stanford.edu>
2015-12-01 18:45:47 -0800

    Better trivial agent

--- minesweeperAgent.py
+++ minesweeperAgent.py
@@ -20,12 +20,14 @@ class minesweeperAgent:
             return False
         def getUnprobedNeighbors(self, node):
             unprobedNeighbors = []
+            numMineNeighbors = 0
             for i in [-1,0,1]:
                 for j in [-1,0,1]:
-
-                    if inBounds((node[0]+i,node[1] + j)) and (node[0]+i,node[1] + j) != node and self.board.whatsAt((node[0]+i,node[1] + j)) != None and (node[0]+i,node[1]+j) in self.unprobed:
+                    if (node[0]+i,node[1]+j) in self.mineLocs:
+                        numMineNeighbors +=1
+                    elif inBounds((node[0]+i,node[1] + j)) and (node[0]+i,node[1] + j) != node and self.board.whatsAt((node[0]+i,node[1] + j)) != None and (node[0]+i,node[1]+j) in self.unprobed:
                         unprobedNeighbors.append((node[0]+i,node[1]+j))
-            return unprobedNeighbors
+            return unprobedNeighbors, numMineNeighbors
         def get_sum_variable(csp, name, variables, maxSum):
             domain = []
             for i in range(0, maxSum+1):
@@ -54,26 +56,52 @@ class minesweeperAgent:
             csp.add_binary_factor(result, beforeFinal, lambda x, y : x == y[1])
             return result
 
-        def combineConstraints(constraints, newConstraint):
+        def combineConstraints(newConstraint):
                 hasChanged = False
                 for i in range(len(constraints)):
                     constraintSet = set(constraints[i][0])
                     newConstraintSet = set(newConstraint[0])
                     if len(constraintSet) > 0 and len(newConstraintSet) > 0:
                         if constraintSet.issubset(newConstraintSet) and constraintSet != newConstraintSet :
-                            combineConstraints(constraints,([node for node in newConstraintSet-constraintSet], newConstraint[1]-constraints[i][1]))
+                            combineConstraints(([node for node in newConstraintSet-constraintSet], newConstraint[1]-constraints[i][1]))
                             hasChanged = True
                         elif newConstraintSet.issubset(constraintSet) and constraintSet != newConstraintSet:
                             constraintValue = constraints[i][1]
                             constraints.remove(constraints[i])
-                            combineConstraints(constraints, ([node for node in constraintSet-newConstraintSet], constraintValue-newConstraint[1]))
+                            combineConstraints(([node for node in constraintSet-newConstraintSet], constraintValue-newConstraint[1]))
 
                 if (hasChanged != True or len(constraints) ==0) and newConstraint not in constraints:
                     constraints.append(newConstraint)
+                    for variable in newConstraint[0]:
+                        mostConstrainedDict[variable] += 1
 
 
+        def checkForTrivialConstraints(self):
+            for constraint in constraints:
+                    if len(constraint[0]) == constraint[1]:
+                        for mine in constraint[0]:
+                            if mine not in self.mineLocs:
+                                self.mineLocs.append(mine)
+                                self.minesRemaining -=1
+                            if mine in self.unprobed:
+                                self.unprobed.remove(mine)
+                        if self.minesRemaining <= 0:
+                            self.solved = True
 
-
+                    if constraint[1] == 0 and len(constraint[0]) > 1:
+                        for node in constraint[0]:
+                            constraints.append(([node], 0))
+                            toProbe.append(node)
+                        constraints.remove(constraint)
+        def checkGameOutcome(self):
+            if self.solved == True:
+                print 'Solution:'
+                solution = minesweeper.Board(self.size, [self.mineLocs[i] for i in range(len(self.mineLocs))])
+                solution.printBoard()
+            if self.lose== True:
+                print 'You lose'
+        def getMCV():
+            return max(mostConstrainedDict.iteritems(), key=lambda x: x[1])
 
         random.seed()
         for i in range(size):
@@ -86,28 +114,38 @@ class minesweeperAgent:
         startNode = (0,0)
         toProbe = []
         toProbe.append(startNode)
+        mostConstrainedDict = collections.Counter()
         while self.solved == False and self.lose == False:
             currentNode = None
             if len(self.unprobed) == 0:
                 self.solved = True
                 break
+            if len(self.unprobed) == self.minesRemaining:
+                for node in self.unprobed:
+                    self.mineLocs.append(node)
+                    self.solved=True
+                break
             if len(toProbe) > 0:
                 currentNode = toProbe[0]
                 nodeValue = board.whatsAt(currentNode)
                 toProbe.remove(currentNode)
+            #elif len(mostConstrainedDict) > 0:
+            #    currentNode = getMCV()[0]
 
             else:
                 currentNode= random.choice(self.unprobed)
                 nodeValue = board.whatsAt(currentNode)
+                print nodeValue
             if nodeValue == 'x':
                 self.lose = True
                 break
-            neighbors = getUnprobedNeighbors(self, currentNode)
+            neighbors, numMineNeighbors = getUnprobedNeighbors(self, currentNode)
+            nodeValue-=numMineNeighbors
             if nodeValue == 0:
                 for neighbor in neighbors:
                     if neighbor not in toProbe:
                         toProbe.append(neighbor)
-                    combineConstraints(constraints, ([neighbor],0))
+                    combineConstraints(([neighbor],0))
             elif nodeValue == len(neighbors):
                 for neighbor in neighbors:
                     self.mineLocs.append(neighbor)
@@ -117,29 +155,11 @@ class minesweeperAgent:
                     self.solved = True
                     break
             else:
-                combineConstraints(constraints, ([neighbor for neighbor in neighbors],nodeValue))
-                for constraint in constraints:
-                    if len(constraint[0]) == constraint[1]:
-                        for mine in constraint[0]:
-                            self.mineLocs.append(mine)
-                            if mine in self.unprobed:
-                                self.unprobed.remove(mine)
-                        self.minesRemaining -= len(constraint[0])
-                        if self.minesRemaining <= 0:
-                         self.solved = True
-                        break
-                    if constraint[1] == 0 and len(constraint[0]) > 1:
-                        for node in constraint[0]:
-                            constraints.append(([node], 0))
-                            toProbe.append(node)
-                        constraints.remove(constraint)
-                print constraints
+                combineConstraints(([neighbor for neighbor in neighbors],nodeValue))
+                checkForTrivialConstraints(self)
+                if self.solved == True:
+                    break
             if currentNode in self.unprobed:
                 self.unprobed.remove(currentNode)
+        checkGameOutcome(self)
 
-        if self.solved == True:
-            print 'Solution:'
-            solution = minesweeper.Board(self.size, [self.mineLocs[i] for i in range(len(self.mineLocs))])
-            solution.printBoard()
-        if self.lose== True:
-            print 'You lose'
--- submission.py
+++ submission.py
@@ -3,8 +3,8 @@ import collections, util, copy, minesweeper, minesweeperAgent
 def main():
     n = 2
     mines = 3
-    board = minesweeper.Board(5,[(2,2)])
+    board = minesweeper.Board(5,[(2,2),(2,3),(1,4),(0,3)])
     board.printBoard()
     mineAI = minesweeperAgent.minesweeperAgent()
-    mineAI.solve(5, 1, board)
+    mineAI.solve(5, 4, board)
 main()
